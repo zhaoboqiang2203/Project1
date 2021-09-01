64840532eb2824be0cc51e8a25eb1f706514c73a
zhaoboqiang2203 <55503776+zhaoboqiang2203@users.noreply.github.com>
2021-09-01 10:15:47 +0800

    Update cath.cpp
    网页修改代码

--- cath.cpp
+++ cath.cpp
@@ -1,15 +1,17 @@
 #include <stdio.h>
-
+#include <iostream>
 int main()
 {
-	int a, b;
+	int a, b，d;
 	
 	int c;
 	a = 10;
 	b = 100;
 	printf("test_code\n");
 	
-	c = a* b;
+	c = a * b;
+	
+	d = a + b;
 	
 	printf("a + b = %d\n", a + b);
 	return 0;
